57291159f392c77b1c246abf76a693f2bf1914d6
Sk8ChainOps <cbarreraperez@outlook.com>
2025-03-26 17:18:19 -0500

    feat: Create dog class and modify animal, main class

---
+++ Dog.java
@@ -0,0 +1,11 @@
+class Dog extends Animal {
+
+    public  Dog(String name, int age, String race){
+        super(name, age, race);
+    }
+
+    public String SoundAnimal(){
+        return "Guau! Guau!";
+    }
+
+}
